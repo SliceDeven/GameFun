b3db57a9c30cb0dcdef7c16afcc45e4a568413e3
SliceDeven <18409252428@163.com>
2020-05-27 18:15:09 +0800

    First Commit

---
+++ src/main/java/com/xupt/blog/gamefunblog/GamefunblogApplication.java
@@ -0,0 +1,13 @@
+package com.xupt.blog.gamefunblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamefunblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamefunblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xupt/blog/gamefunblog/GamefunblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xupt.blog.gamefunblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamefunblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
